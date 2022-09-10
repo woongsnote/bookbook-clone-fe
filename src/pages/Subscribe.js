7a1990ae6267a77f9894003cb405b245802a732b
MIINII <ft.dawn.o@gmail.com>
2022-09-11 03:10:34 +0900

    :lipstick: [SUB] profile 만들기

--- src/pages/Subscribe.js
+++ src/pages/Subscribe.js
@@ -1,8 +1,80 @@
-const Subscribe =() => {
-  return(
-    <>
-    </>
-  )
-}
+import Header from './../components/common/Header';
+import Layout from './../components/layout/Layout';
+import tw from 'tailwind-styled-components';
+import styled from 'styled-components';
 
-export default Subscribe
+import profile from '../image/profile.jpg';
+import { useState } from 'react';
+
+const Subscribe = () => {
+  const [Nickname, setNickName] = useState('췤키라웃');
+  const [Sub, setSub] = useState('구독중');
+
+  return (
+    <>
+      <Header />
+      <Layout>
+        <div>
+          <ProfileImg>
+            <img src={profile} alt='bookimg' />
+          </ProfileImg>
+          <ProfileInfo>
+            <ProfileId>{Nickname}</ProfileId>
+            <IsSub>{Sub}</IsSub>
+          </ProfileInfo>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+// const Profile = tw.div`
+//   w-profile
+//   h-profile
+//   bg-slate-700
+//   rounded-full
+
+//   & > img {
+//     w-full
+//     h-full
+//     object-cover
+//   }
+// `;
+
+const ProfileImg = styled.div`
+  margin: auto;
+  width: 300px;
+  height: 300px;
+  border-radius: 100%;
+  overflow: hidden;
+
+  & img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ProfileInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+`;
+const ProfileId = styled.div`
+  color: #3a3a3a;
+  font-size: 30px;
+  font-weight: 700;
+  text-align: center;
+  margin-right: 10px;
+`;
+const IsSub = styled.div`
+  border: 0.5px solid #567F9E;
+  border-radius: 20px;
+  padding: 3px 7px;
+  font-size: 12px;
+  font-weight: 600;
+  color: #567F9E;
+`;
+
+export default Subscribe;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,18 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     colors: {
-      'Bwhite': '#ffffff',
-      'Bred': '#D34242',
-      'Bblue': '#567F9E',
-      'Bblack': '#3A3A3A',
+      Bwhite: '#ffffff',
+      Bred: '#D34242',
+      Bblue: '#567F9E',
+      Bblack: '#3A3A3A',
     },
     extend: {
       width: {
         c: '45rem',
+        profile: '200px',
+      },
+      height: {
+        profile: '200px',
       },
     },
   },
