667e4fc8559c7b54dcef037c36f76f9991003f7c
MIINII <ft.dawn.o@gmail.com>
2022-09-14 18:08:23 +0900

    :lipstick: [HEADER] 프로필 이미지 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,23 +1,24 @@
-import React from "react";
-import tw from "tailwind-styled-components";
-import styled from "styled-components";
-import { BiMenu } from "react-icons/bi";
-import { IoClose } from "react-icons/io5";
-import logo from "../../image/typo.png";
-import Logo from "./Logo";
-import SideBar from "./SideBar";
+import React from 'react';
+import tw from 'tailwind-styled-components';
+import styled from 'styled-components';
+import { BiMenu } from 'react-icons/bi';
+import { IoClose } from 'react-icons/io5';
+import logo from '../../image/typo.png';
+import Logo from './Logo';
+import SideBar from './SideBar';
+import profile from '../../image/profile.jpg';
 
 const Header = () => {
   const [spreadNav, setSpreadNav] = React.useState(false); // 네비바를 여는 스테이트.
   const [isView, setIsView] = React.useState(false); // 내부 값을 보이게 하는 스테이트.
-  const [bgView, setBgView] = React.useState("transparent");
+  const [bgView, setBgView] = React.useState('transparent');
 
-  const [id, setId] = React.useState("췤키라웃");
-  const [img, setImg] = React.useState({});
+  const [id, setId] = React.useState('췤키라웃');
+  const [img, setImg] = React.useState(profile);
 
   const onClick = () => {
-    setSpreadNav((props) => !props);
-    setTimeout(() => setIsView((props) => !props), 350);
+    setSpreadNav(props => !props);
+    setTimeout(() => setIsView(props => !props), 350);
   };
 
   // const onChangeBg = () => {
@@ -26,34 +27,18 @@ const Header = () => {
   return (
     <HeaderBox>
       <MenuWrap>
-        <MenuButton onClick={onClick}>
-          {!spreadNav ? (
-            <BiMenu fontSize="30px" color="#3a3a3a" />
-          ) : (
-            <IoClose fontSize="30px" color="#3a3a3a" />
-          )}
-        </MenuButton>
+        <MenuButton onClick={onClick}>{!spreadNav ? <BiMenu fontSize='30px' color='#3a3a3a' /> : <IoClose fontSize='30px' color='#3a3a3a' />}</MenuButton>
         <SideBar spreadNav={spreadNav} isView={isView} />
-        <MenuButton onClick={onClick}>
-          {!spreadNav ? (
-            <BiMenu fontSize="30px" color="#3a3a3a" />
-          ) : (
-            <IoClose fontSize="30px" color="#fff" />
-          )}
-        </MenuButton>
+        <MenuButton onClick={onClick}>{!spreadNav ? <BiMenu fontSize='30px' color='#3a3a3a' /> : <IoClose fontSize='30px' color='#fff' />}</MenuButton>
       </MenuWrap>
 
       <Logo>
-        <img src={logo} alt="logo" />
+        <img src={logo} alt='logo' />
       </Logo>
 
       <UserInfo>
-        <UserImg className="w-3 h-3 rounded-full hidden">
-          <img
-            className="w-full h-full object-cover bg-slate-600"
-            src={setImg}
-            alt="profile-img"
-          />
+        <UserImg>
+          <img className='w-full h-full object-cover' src={img} alt='profile-img' />
         </UserImg>
         <UserId>{id}</UserId>
       </UserInfo>
@@ -86,15 +71,18 @@ const MenuButton = styled.div`
 `;
 
 const UserInfo = tw.div`
-  w-20
+  w-32
+  flex
+  items-center
 `;
 const UserImg = tw.div`
-w-3 h-3 rounded-full hidden
+w-12 h-12 rounded-full overflow-hidden
 
-& img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+& > img {
+  w-12
+  h-12
+  mr-3
+  object-cover
 }
 `;
 const UserId = tw.div``;
