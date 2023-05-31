9bdbfb65fb26373a3084f9ae5193be3afa33249a
woongsnote <woongsnote@gmail.com>
2023-05-31 11:46:40 +0900

    [Feat] 메인 화면 이동 추가

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,10 +1,15 @@
+import { useNavigate } from "react-router-dom";
 
 const logo = require("../../image/typo.png");
 
 const Logo = () => {
+  const navigate = useNavigate();
+  const goMain = () => {
+    navigate("/");
+  };
   return (
     <div className="w-auto">
-      <img src={logo} alt="logo"  className="w-48" />
+      <img src={logo} alt="logo" className="w-48 hover:cursor-pointer" onClick={goMain} />
     </div>
   );
 };
