cb7bf3f6422d8e3e688820a4c72775888effaa9c
woongsnote <woongsnote@gmail.com>
2023-05-31 17:42:07 +0900

    [Fix] CSS 방식 통일

--- src/components/post/BookIntro.tsx
+++ src/components/post/BookIntro.tsx
@@ -1,34 +1,24 @@
-// import styled from "styled-components";
-import { useSelector } from "react-redux";
+import { Dispatch, SetStateAction } from "react";
 
-const BookIntro = ({ comment, setComment }: any) => {
-  const review = useSelector((state: any) => state.postSlice.post);
+type BookIntroProps = {
+  comment: string;
+  setComment: Dispatch<SetStateAction<string>>;
+};
+
+const BookIntro = ({ comment, setComment }: BookIntroProps) => {
   return (
-    <div>
+    <div className="flex flex-col mt-[10%]">
       <span className="text-black text-xl font-bold">📖 책 소개 📖</span>
       <textarea
-        name="booktintro"
+        className="w-[80%]"
+        name="book_intro"
         onChange={(e) => {
           setComment(e.target.value);
         }}
-        placeholder={review.comment}
+        placeholder={comment}
       />
     </div>
   );
 };
-
-// const BookIntroBox = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   margin-top: 10%;
-//   justify-content: center;
-//   align-self: center;
-//   & > textarea {
-//     min-width: 80%;
-//     min-height: 9.375rem;
-//     border: 0.5px solid rgba(58, 58, 58, 0.3);
-//     box-shadow: 0px 15px 25px -4px rgba(150, 150, 150, 0.24);
-//   }
-// `;
 
 export default BookIntro;
