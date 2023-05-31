eef4feecf7cd659bc00410c7096918ca7bce26e3
woongsnote <woongsnote@gmail.com>
2023-05-31 17:34:19 +0900

    [Feat] 공통 컴포넌트 추가

---
+++ src/components/common/PageContainer.tsx
@@ -0,0 +1,5 @@
+const PageContainer = ({ children }: { children: React.ReactNode }) => {
+  return <div className="mx-auto max-w-2xl pt-28 h-full">{children}</div>;
+};
+
+export default PageContainer;
