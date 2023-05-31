f4ed5eba2d36c6ceb813a0a92a3e429bfbce7d2b
woongsnote <woongsnote@gmail.com>
2023-05-31 17:28:06 +0900

    [Fix] 버튼 파일 위치 변경

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,13 +1,14 @@
-const Button = ({
-  props,
-  children,
-}: {
+type ButtonProps = {
   props?: any;
   children: React.ReactNode;
-}) => {
+  onClick: () => void;
+};
+
+const Button = ({ props, children, onClick }: ButtonProps) => {
   return (
     <button
       {...props}
+      onClick={onClick}
       className="block bg-Bblue active:bg-gray-600 focus-visible:ring ring-gray-300 text-white text-sm md:text-base font-semibold
       text-center rounded-lg outline-none transition duration-100 px-8 py-3 disabled:opacity-75 disabled:cursor-none">
       {children}
