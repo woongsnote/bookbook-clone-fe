01cde9bc5144e411f07b11c68f93c81a65fd8c5b
woongsnote <woongsnote@gmail.com>
2023-05-31 17:37:20 +0900

    [Feat] 검색 결과 타입 추가 및 style 추가

--- src/components/search/SearchedBook.tsx
+++ src/components/search/SearchedBook.tsx
@@ -1,21 +1,30 @@
 import { useNavigate } from "react-router-dom";
 
-const SearchedBook = ({ title, author, imageUrl, content }: any) => {
+type BookProps = {
+  title: string;
+  author: string;
+  imageUrl: string;
+  content: string;
+};
+
+const SearchedBook = ({ title, author, imageUrl, content }: BookProps) => {
   const navigate = useNavigate();
   const goDetail = () => {
     navigate("/post", { state: { title, imageUrl } });
   };
 
   return (
-    <div onClick={goDetail} className="">
-      <img src={imageUrl} alt="BookCover" className="" />
-      <div>
-        <span>{title}</span>
+    <div
+      onClick={goDetail}
+      className="flex flex-row rounded-lg bg-white border shadow-lg m-2 hover:cursor-pointer">
+      <img src={imageUrl} alt="BookCover" className="object-none m-2" />
+      <div className="flex flex-col justify-center">
+        <h3 className="font-bold text-sm md:text-lg mb-1">{title}</h3>
 
-        <span>{author}</span>
+        <span className="my-1 font-bold">{author}</span>
 
-        <p>
-          {content.length < 100 ? content : content.slice(0, 80) + "..."}
+        <p className="">
+          {content.length < 200 ? content : content.slice(0, 152) + "..."}
         </p>
       </div>
     </div>
--- src/components/search/SearchedList.tsx
+++ src/components/search/SearchedList.tsx
@@ -1,24 +1,27 @@
+import { Book } from "../../types/types";
+import EmptyList from "../common/EmptyList";
 import SearchedBook from "./SearchedBook";
 
 type SearchedListProps = {
-  searchList: any[];
+  searchList: Book[];
 };
 const SearchedList = ({ searchList }: SearchedListProps) => {
+  if (searchList === null) return <EmptyList />;
+
   return (
-    <>
-      {searchList &&
-        searchList.map((book: any) => {
-          return (
-            <SearchedBook
-              key={book.isbn}
-              author={book.authors[0]}
-              title={book.title}
-              content={book.contents}
-              imageUrl={book.thumbnail}
-            />
-          );
-        })}
-    </>
+    <section>
+      {searchList.map((book: Book) => {
+        return (
+          <SearchedBook
+            key={book.isbn}
+            author={book.authors[0]}
+            title={book.title}
+            content={book.contents}
+            imageUrl={book.thumbnail}
+          />
+        );
+      })}
+    </section>
   );
 };
 
