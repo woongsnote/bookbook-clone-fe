38721560589ab1aa85c07d01503e43f7284832d3
woongsnote <woongsnote@gmail.com>
2023-06-01 17:04:35 +0900

    [Refactor] 독서 기간 컴포넌트 타입 수정

--- src/components/post/ReadingPeriod.tsx
+++ src/components/post/ReadingPeriod.tsx
@@ -1,10 +1,41 @@
-import DatePick from './DatePick';
+import { Dispatch, SetStateAction } from "react";
 
-const ReadingPeriod = ({ readStart, readEnd }:any) => {
+type ReadingPeriodProps = {
+  readStart: Date | string | undefined;
+  readEnd: Date | string | undefined;
+  setReadStart: Dispatch<SetStateAction<string | Date>>;
+  setReadEnd: Dispatch<SetStateAction<string | Date>>;
+};
+
+const ReadingPeriod = ({
+  readStart,
+  readEnd,
+  setReadStart,
+  setReadEnd,
+}: ReadingPeriodProps) => {
   return (
     <div>
-      <span className='text-black text-xl font-bold'>독서 기간</span>
-      <DatePick />
+      <h2>독서 기간</h2>
+      <input
+        type="date"
+        id="readStart"
+        min="2000-01-01"
+        max="2999-12-31"
+        value={readStart?.toString()}
+        onChange={(e) => {
+          setReadStart(e.target.value);
+        }}
+      />
+      <input
+        type="date"
+        id="readEnd"
+        min="2000-01-01"
+        max="2999-12-31"
+        value={readEnd?.toString()}
+        onChange={(e) => {
+          setReadEnd(e.target.value);
+        }}
+      />
     </div>
   );
 };
