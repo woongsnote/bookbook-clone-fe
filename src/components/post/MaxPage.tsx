746fba3db5f1a9a71a14407d2f901b818df23b23
woongsnote <woongsnote@gmail.com>
2023-06-01 17:25:48 +0900

    [Refactor] 최대 페이지 컴포넌트 타입 수정

--- src/components/post/MaxPage.tsx
+++ src/components/post/MaxPage.tsx
@@ -1,40 +1,28 @@
-// import styled from "styled-components";
-import { useSelector } from "react-redux";
+import { ChangeEvent, Dispatch, SetStateAction } from "react";
 
-const MaxPage = ({ page, setPage }: any) => {
-  const review = useSelector((state: any) => state.postSlice.post);
+type MaxPageProps = {
+  page: number;
+  setPage: Dispatch<SetStateAction<number>>;
+};
 
-  const onChangePage = (e: any) => {
-    console.log("🚀 ~ onChangePage ~ setPage", page);
-    setPage(e.target.value);
+const MaxPage = ({ page, setPage }: MaxPageProps) => {
+  const onChangePage = (e: ChangeEvent<HTMLInputElement>) => {
+    setPage(+e.target.value);
   };
 
   return (
-    <div>
-      <span className="text-black text-xl font-bold"> 📖페이지</span>
-      <input
-        type="number"
-        name="publisher"
-        onChange={onChangePage}
-        placeholder={review.page}
-      />
+    <div className="mt-10">
+      <div className="flex flex-col items-start">
+        <span className="text-black text-xl font-bold"> 📖페이지</span>
+        <input
+          type="number"
+          className="w-[150px] rounded-[15px]"
+          onChange={onChangePage}
+          placeholder={page.toString()}
+        />
+      </div>
     </div>
   );
 };
-
-// const BookIntroBox = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   align-items: flex-start;
-//   /* justify-content: space-between; */
-//   margin-top: 10%;
-//   & input {
-//     width: 150px;
-//     border: 0.5px solid rgba(58, 58, 58, 0.3);
-//     border-radius: 15px;
-//     background-color: #fff;
-//     box-shadow: 0px 15px 25px -4px rgba(150, 150, 150, 0.24);
-//   }
-// `;
 
 export default MaxPage;
